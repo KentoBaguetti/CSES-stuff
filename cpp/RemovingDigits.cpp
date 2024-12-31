b1e831f8807b87eee8d09c0f5adc0eb2996b61f9
Kentaro Barnes <kentaro.barnes5@gmail.com>
2024-12-30 19:50:13 -0800

    Rd

---
+++ cpp/RemovingDigits.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve()
+{
+
+    int n;
+    cin >> n;
+
+    int count = 0;
+
+     // while
+
+}
+
+int32_t main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    solve();
+}

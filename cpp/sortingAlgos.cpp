0023a770d51d3c4f0663ef49fdc583948c83a38c
Kentaro Barnes <kentaro.barnes5@gmail.com>
2025-01-24 23:31:24 -0800

    sorting

--- cpp/sortingAlgos.cpp
+++ cpp/sortingAlgos.cpp
@@ -1,37 +1,82 @@
-#include <bits/stdc++.h>
+// #include <bits/stdc++.h>
+#include <vector>
+#include <algorithm>
+#include <iostream>
 using namespace std;
 
-int indexOfMin(vector<int>& arr, int a) {
-    int m = a;
-    for (int i = a + 1; i < arr.size(); i++) {
-        if (arr[i] < arr[m]) {
-            m = i;
-        }
-    }
-    return m;
-}
+// int indexOfMin(vector<int>& arr, int a) {
+//     int m = a;
+//     for (int i = a + 1; i < arr.size(); i++) {
+//         if (arr[i] < arr[m]) {
+//             m = i;
+//         }
+//     }
+//     return m;
+// }
+
+// vector<int> selectionSort(vector<int>& arr) {
+//     for (int i = 0; i < arr.size(); i++) {
+//         int minValIndex = i;
+//         for (int j = i + 1; j < arr.size(); j++) {
+//             if (arr[j] < arr[minValIndex]) {
+//                 minValIndex = j;
+//             }
+//         }
+//         int temp = arr[minValIndex];
+//         arr[minValIndex] = arr[i];
+//         arr[i] = temp;
+//     }
+//     return arr;
+// }
+
+// vector<int> selectionSortWithHelper(vector<int>& arr) {
+//     for (int i = 0; i < arr.size(); i++) {
+//         int minIndex = indexOfMin(arr, i);
+//         int temp = arr[i];
+//         arr[i] = arr[minIndex];
+//         arr[minIndex] = temp;
+//     }
+//     return arr;
+// }
+
+// vector<int> insertionSort(vector<int>& arr) {
+//     for (int i = 1; i < arr.size(); i++) {
+//         int j = i - 1;
+//         int val = arr[i];
+//         while (j >= 0 && arr[j] > val) {
+//             arr[j + 1] = arr[j];
+//             arr[j] = val;
+//             j--;
+//         }
+//     }
+//     return arr;
+// }
+
+// vector<int> otherInsertionSort(vector<int>& arr) {
+//     for (int i = 1; i < arr.size(); i++) {
+//         int val = arr[i];
+//         int j = i - 1;
+//         while (j >= 0 && arr[j] > val) {
+//             arr[j + 1] = arr[j];
+//             j--;
+//         }
+//         arr[j + 1] = val;
+//     }
+//     return arr;
+// }
 
 vector<int> selectionSort(vector<int>& arr) {
     for (int i = 0; i < arr.size(); i++) {
-        int minValIndex = i;
-        for (int j = i + 1; j < arr.size(); j++) {
-            if (arr[j] < arr[minValIndex]) {
-                minValIndex = j;
-            }
+      int minIndex = i;
+      for (int j = i + 1; j < arr.size(); j++) {
+        if (arr[j] < arr[minIndex]) {
+            minIndex = j;
         }
-        int temp = arr[minValIndex];
-        arr[minValIndex] = arr[i];
-        arr[i] = temp;
-    }
-    return arr;
-}
+      }
+      int temp = arr[i];
+      arr[i] = arr[minIndex];
+      arr[minIndex] = temp;
 
-vector<int> selectionSortWithHelper(vector<int>& arr) {
-    for (int i = 0; i < arr.size(); i++) {
-        int minIndex = indexOfMin(arr, i);
-        int temp = arr[i];
-        arr[i] = arr[minIndex];
-        arr[minIndex] = temp;
     }
     return arr;
 }
@@ -40,20 +85,7 @@ vector<int> insertionSort(vector<int>& arr) {
     for (int i = 1; i < arr.size(); i++) {
         int j = i - 1;
         int val = arr[i];
-        while (j >= 0 && arr[j] > val) {
-            arr[j + 1] = arr[j];
-            arr[j] = val;
-            j--;
-        }
-    }
-    return arr;
-}
-
-vector<int> otherInsertionSort(vector<int>& arr) {
-    for (int i = 1; i < arr.size(); i++) {
-        int val = arr[i];
-        int j = i - 1;
-        while (j >= 0 && arr[j] > val) {
+        while ( j >= 0 && arr[j] > val) {
             arr[j + 1] = arr[j];
             j--;
         }
@@ -62,11 +94,21 @@ vector<int> otherInsertionSort(vector<int>& arr) {
     return arr;
 }
 
+int fun(int n) {
+    int res = 8;
+    int j = 2;
+    while (j < n) {
+        res *= 5;
+        j++;
+    }
+    return res;
+}
+
 int main() {
 
     vector<int> arr(30);
     generate(arr.begin(), arr.end(), []() { return rand() % 100; });
-    vector<int> sortedList = otherInsertionSort(arr);
+    vector<int> sortedList = insertionSort(arr);
 
     cout << "[";
     for (int i = 0; i < sortedList.size() - 1; i++) {

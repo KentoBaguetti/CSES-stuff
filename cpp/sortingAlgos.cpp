e5cb1e7a2e3d9c6349844267a99c2193c4edd38f
Kentaro Barnes <kentaro.barnes5@gmail.com>
2025-01-30 23:47:33 -0800

    merge sort

--- cpp/sortingAlgos.cpp
+++ cpp/sortingAlgos.cpp
@@ -104,11 +104,97 @@ int fun(int n) {
     return res;
 }
 
+// void Merge(vector<int> &arr, int left, int mid, int right) {
+//     int first1 = left;
+//     int first2 = mid + 1;
+//     int last1 = mid;
+//     int last2 = right;
+//     vector<int> temp;
+//     int index = 0;
+
+//     while ((first1 <= last1) && (first2 <= last2)) {
+//         if (arr[first1] <= arr[first2]) {
+//             temp.push_back(arr[first1++]);
+//         } else {
+//             temp.push_back(arr[first2++]);
+//         }
+//     }
+
+//     while (first1 <= last1)
+//         temp.push_back(arr[first1++]);
+//     while (first2 <= last2)
+//         temp.push_back(arr[first2++]);
+
+//     for (int i = left; i <= right; i++) {
+//         arr[i] = temp[index++];
+//     }
+
+// }
+
+// vector<int> MS(vector<int> &arr, int left, int right) {
+//     if (left < right) {
+
+//         int mid = (left + right) / 2;
+
+//         MS(arr, left, mid);
+//         MS(arr, mid + 1, right);
+//         Merge(arr, left, mid, right);
+//     }
+//     return arr;
+// }
+
+void Merge(vector<int>& arr, int left, int mid, int right) {
+
+    int first1 = left;
+    int first2 = mid + 1;
+    int last1 = mid;
+    int last2 = right;
+
+    vector<int> temp;
+    int tempIndex = 0;
+
+    while ((first1 <= last1) && (first2 <= last2)) {
+        if (arr[first1] <= arr[first2])
+            temp.push_back(arr[first1++]);
+        else
+            temp.push_back(arr[first2++]);
+    }
+
+    while (first1 <= last1)
+        temp.push_back(arr[first1++]);
+    while (first2 <= last2)
+        temp.push_back(arr[first2++]);
+
+    for (int i = left; i <= right; i++) {
+        arr[i] = temp[tempIndex++];
+    }
+
+}
+
+vector<int> MS(vector<int>& arr, int left, int right) {
+    if (left < right) {
+
+        int mid = (left + right) / 2;
+
+        MS(arr, left, mid);
+        MS(arr, mid + 1, right);
+
+        Merge(arr, left, mid, right);
+
+    }
+    return arr;
+}
+
+vector<int> MergeSort(vector<int> &arr) {
+    return MS(arr, 0, arr.size()-1);
+}
+
 int main() {
 
-    vector<int> arr(30);
-    generate(arr.begin(), arr.end(), []() { return rand() % 100; });
-    vector<int> sortedList = insertionSort(arr);
+    // vector<int> arr(30);
+    // generate(arr.begin(), arr.end(), []() { return rand() % 100; });
+    vector<int> arr = {5,2,3,7,1,2,3,4,56,7,100,-1,6,4};
+    vector<int> sortedList = MergeSort(arr);
 
     cout << "[";
     for (int i = 0; i < sortedList.size() - 1; i++) {
